be9ad2e6d0f61e162e1f1f20cbb00bc3863745d8
Ryan Nóbrega <ryanbrandao18@gmail.com>
2022-02-22 19:23:34 -0300

    atualizando

--- app/js/model/conta.js
+++ app/js/model/conta.js
@@ -1,28 +1,28 @@
 class Conta {
-    constructor(numero, saldo = 0) {
-        this._numero = numero;
-        this._saldo = saldo;
+  constructor(numero, saldo = 0) {
+    this._numero = numero
+    this._saldo = saldo
+  }
+  get numero() {
+    return this._numero
+  }
+  set numero(numero) {
+    this._numero = numero
+  }
+  get saldo() {
+    return this._saldo
+  }
+  debitar(valor) {
+    //apenas debita se houver saldo
+    if (valor < this._saldo) {
+      this._saldo -= valor
     }
-    get numero() {
-        return this._numero;
-    }
-    set numero(numero) {
-        this._numero = numero;
-    }
-    get saldo() {
-        return this._saldo;
-    }
-    debitar(valor) {
-        //apenas debita se houver saldo
-        if (valor < this._saldo) {
-            this._saldo -= valor;
-        }
-    }
-    creditar(valor) {
-        this._saldo += valor;
-    }
-    toString() {
-        return `Número: ${this._numero} 
-        - Saldo: ${this._saldo}`;
-    }
+  }
+  creditar(valor) {
+    this._saldo += valor
+  }
+  toString() {
+    return `Número: ${this._numero} 
+        - Saldo: ${this._saldo}`
+  }
 }
